9a3e64d3507bd0378bd2c8474eb4f0516fff64e3
Rob <684692+robvdw@users.noreply.github.com>
2024-04-15 21:38:01 +0200

    novo

---
+++ respec/test/whitepaper/config.js
@@ -0,0 +1,67 @@
+var respecConfig = {
+  specStatus: "GN-VV",
+  specType: "IM",
+  pubDomain: "bin",
+  //canonicalURI: "WV",
+  editors: [
+    {
+      name: "Linda van den Brink",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/",
+    },
+    {
+      name: "Friso Penninga",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/",
+    },
+  ],
+  previousPublishDate: "2014-05-01",
+  previousStatus: "GN-CV",
+  previousType: "IM",
+  shortName: "whitepaper",
+  github: "https://github.com/Geonovum/whitepaper-standaarden",
+  localBiblio: {
+    ISO19136: {
+      href: "https://www.iso.org/standard/32554.html",
+      title:
+        "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+      authors: [""],
+      date: "September 2007",
+      publisher: "International Organization for Standardization",
+    },
+    ISO19101: {
+      href: "https://www.iso.org/standard/26002.html",
+      title: "ISO 19101:2002 Geographic information -- Reference model",
+      authors: [""],
+      date: "Juli 2002",
+      publisher: "International Organization for Standardization",
+    },
+    ISO19115: {
+      href: "https://www.iso.org/standard/26020.html",
+      title: "ISO 19115:2003  Geographic information -- Metadata",
+      authors: [""],
+      date: "Mei 2003",
+      publisher: "International Organization for Standardization",
+    },
+    ISO19139: {
+      href:
+        "http://inspire.ec.europa.eu/documents/Metadata/INSPIRE_MD_IR_and_ISO_v1_2_20100616.pdf",
+      title:
+        " ISO/TS 19139:2007 Geographic information -- Metadata -- XML schema implementation",
+      authors: [""],
+      date: "April 2007",
+      publisher: "International Organization for Standardization",
+    },
+    "INSPIRE-19115-19119": {
+      href:
+        "http://inspire.ec.europa.eu/documents/Metadata/INSPIRE_MD_IR_and_ISO_v1_2_20100616.pdf",
+      title:
+        "INSPIRE Metadata Implementing Rules: Technical Guidelines based on EN ISO 19115 and EN ISO 19119",
+      authors: [
+        "Drafting Team Metadata and European Commission Joint Research Centre",
+      ],
+      date: "26 oktober 2007",
+      publisher: "European Commission Joint Research Centre",
+    },
+  },
+};
